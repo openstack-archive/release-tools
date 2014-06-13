5c0ab045057730d1be08ed693356ec02deaaf031
Thierry Carrez <thierry@openstack.org>
2014-06-13 12:02:29 +0200

    Support swift and oslo milestone releases
    For intermediary releases of Swift, first a RC tag is pushed on master
    and bugs get fixreleased (using swiftrc.sh) then we use milestone.sh
    in SKIPBUGS mode to push the final tag and upload the resulting tarball
    without affecting bugs.
    
    oslo-incubator does not produce tarballs, but we still want tags and
    mark the milestone "released". We use milestone.sh in SKIPTARBALL mode
    to just skip tarball wait and call upload_release.py with the --nop
    argument to skip tarball upload.
    
    oslo.messaging is not tagged at milestones, but we still want to mark
    the milestone "released". We use milestone.sh in SKIPTARBALL and SKIPTAG
    mode to skip tagging completely, skip tarball wait and call
    upload_release.py with the --nop argument to skip tarball upload.
    
    Change-Id: I7e670d2dd5052460a593a06e82dc212d176a395d

--- milestone.sh
+++ milestone.sh
@@ -37,47 +37,74 @@ function title {
   echo "$(tput bold)$(tput setaf 1)[ $1 ]$(tput sgr0)"
 }
 
+if [[ "$PROJECT" == "swift" ]]; then
+  echo "Swift mode: skipping fixreleasing (bugs should be set at RC time)"
+  SKIPBUGS=1
+fi
+
+if [[ "$PROJECT" == "oslo-incubator" ]]; then
+  echo "Oslo-incubator mode: skipping tarball generation and upload"
+  SKIPTARBALL=1
+fi
+
+if [[ "$PROJECT" == "oslo.messaging" ]]; then
+  echo "oslo.messaging mode: skipping tag, tarball generation and upload"
+  SKIPTAG=1
+  SKIPTARBALL=1
+fi
+
 title "Resolving $MILESTONE to version"
 VERSION=`$TOOLSDIR/ms2version.py $PROJECT $MILESTONE`
 RELVERSION=${VERSION:0:6}
 echo "$MILESTONE is $VERSION (final being $RELVERSION)"
 
-title "Cloning repository for $PROJECT"
-MYTMPDIR=`mktemp -d`
-cd $MYTMPDIR
-git clone git://git.openstack.org/openstack/$PROJECT
-cd $PROJECT
-LANG=C git review -s
-TARGETSHA=`git log -1 $SHA --format='%H'`
-HEADSHA=`git log -1 HEAD --format='%H'`
+if [[ "$SKIPTAG" != "1" ]]; then
+  title "Cloning repository for $PROJECT"
+  MYTMPDIR=`mktemp -d`
+  cd $MYTMPDIR
+  git clone git://git.openstack.org/openstack/$PROJECT
+  cd $PROJECT
+  LANG=C git review -s
+  TARGETSHA=`git log -1 $SHA --format='%H'`
+  HEADSHA=`git log -1 HEAD --format='%H'`
 
-title "Tagging $TARGETSHA as $VERSION"
-TAGMSG="${PROJECT^} $MILESTONE milestone ($VERSION)"
-echo "Tag message is '$TAGMSG'"
-if [[ "$TARGETSHA" != "$HEADSHA" ]]; then
-  echo "Warning: target SHA does not correspond to HEAD"
+  title "Tagging $TARGETSHA as $VERSION"
+  TAGMSG="${PROJECT^} $MILESTONE milestone ($VERSION)"
+  echo "Tag message is '$TAGMSG'"
+  if [[ "$TARGETSHA" != "$HEADSHA" ]]; then
+    echo "Warning: target SHA does not correspond to HEAD"
+  fi
+  git tag -m "$TAGMSG" -s "$VERSION" $TARGETSHA
+  git push gerrit $VERSION
+  REALSHA=`git show-ref -s "$VERSION"`
+
+  title "Cleaning up"
+  cd ../..
+  rm -rf $MYTMPDIR
 fi
-git tag -m "$TAGMSG" -s "$VERSION" $TARGETSHA
-git push gerrit $VERSION
-REALSHA=`git show-ref -s "$VERSION"`
 
-title "Waiting for tarball from $REALSHA"
-$TOOLSDIR/wait_for_tarball.py $REALSHA
+if [[ "$SKIPTARBALL" != "1"]]; then
+  title "Waiting for tarball from $REALSHA"
+  $TOOLSDIR/wait_for_tarball.py $REALSHA
 
-title "Checking tarball is similar to last master.tar.gz"
-if [[ "$TARGETSHA" != "$HEADSHA" ]]; then
-  echo "It will probably be a bit different since target is not HEAD."
+  title "Checking tarball is similar to last master.tar.gz"
+  if [[ "$TARGETSHA" != "$HEADSHA" ]]; then
+    echo "It will probably be a bit different since target is not HEAD."
+  fi
+  $TOOLSDIR/similar_tarballs.sh $PROJECT master $VERSION
+  read -sn 1 -p "Press any key to continue..."
 fi
-$TOOLSDIR/similar_tarballs.sh $PROJECT master $VERSION
-read -sn 1 -p "Press any key to continue..."
 
-title "Setting FixCommitted bugs to FixReleased"
-$TOOLSDIR/process_bugs.py $PROJECT --settarget=$MILESTONE --fixrelease
-read -sn 1 -p "Fix any leftover bugs manually and press key to continue..."
+if [[ "$SKIPBUGS" != "1" ]]; then
+  title "Setting FixCommitted bugs to FixReleased"
+  $TOOLSDIR/process_bugs.py $PROJECT --settarget=$MILESTONE --fixrelease
+  read -sn 1 -p "Fix any leftover bugs manually and press key to continue..."
+fi
 
-title "Uploading tarball to Launchpad"
-$TOOLSDIR/upload_release.py $PROJECT $RELVERSION --milestone=$MILESTONE
-
-title "Cleaning up"
-cd ../..
-rm -rf $MYTMPDIR
+if [[ "$SKIPTARBALL" != "1" ]]; then
+  title "Uploading tarball to Launchpad"
+  $TOOLSDIR/upload_release.py $PROJECT $RELVERSION --milestone=$MILESTONE
+else
+  title "Marking milestone as released in Launchpad"
+  $TOOLSDIR/upload_release.py $PROJECT $RELVERSION --milestone=$MILESTONE --nop
+fi
