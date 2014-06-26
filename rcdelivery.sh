e41309d63dc2beb346af7cac07e7a3aa9c30dbbb
Thierry Carrez <thierry@openstack.org>
2014-06-26 15:45:49 +0200

    Support proposed/* branches in release scripts
    mpcut.sh and mpdelivery.sh scripts were used to cut milestone-proposed
    branches for milestones and pre-release, and then tag and upload
    development milestones, RCs and final releases.
    
    This change replaces them by a rccut.sh script which will cut a
    proposed/foo branch for pre-release (at RC1 time), and a rcdelivery.sh
    script which will tag and upload RCs and final releases.
    
    These new scripts also take simpler input than the old versions, by
    taking advantage of ms2version.py to calculate version numbers and
    validate milestone pages (although in the swift case, we still rely on
    a command-line argument to convert series to version number).
    
    Change-Id: Ied44469881caa1f9d1fd2035d2f2f8bfae7f2885

---
+++ rcdelivery.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+#
+# Script to publish RCs (and final release) from proposed/foo in one shot
+#
+# Copyright 2011-2014 Thierry Carrez <thierry@openstack.org>
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+set -e
+
+if [ $# -lt 3 ]; then
+    echo "Usage: $0 series rcX|final projectname [swift_final_version]"
+    echo
+    echo "Example: $0 juno final keystone"
+    echo "Example: $0 juno rc2 swift 2.1.0"
+    exit 2
+fi
+
+SERIES=$1
+RC=$2
+PROJECT=$3
+if [ "$PROJECT" == "swift" ]; then
+  if [ $# -eq 4 ]; then
+    FINALVERSION=$4
+  else
+    echo "Missing Swift final version number argument !"
+    exit 2
+  fi
+fi
+
+TOOLSDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+function title {
+  echo
+  echo "$(tput bold)$(tput setaf 1)[ $1 ]$(tput sgr0)"
+}
+
+title "Resolving $PROJECT $SERIES $RC to version"
+
+if [ "$RC" == "final" ]; then
+  if [ "$PROJECT" != "swift" ]; then
+    RC1VERSION=`$TOOLSDIR/ms2version.py $PROJECT $SERIES-rc1`
+    FINALVERSION=${RC1VERSION:0:6}
+  fi
+  MILESTONE=$FINALVERSION
+  VERSION=$FINALVERSION
+  $TOOLSDIR/ms2version.py --onlycheck $PROJECT $MILESTONE
+else
+  if [ "$PROJECT" != "swift" ]; then
+    MILESTONE="$SERIES-$RC"
+    VERSION=`$TOOLSDIR/ms2version.py $PROJECT $MILESTONE`
+  else
+    MILESTONE="$FINALVERSION-$RC"
+    VERSION="$FINALVERSION.$RC"
+    $TOOLSDIR/ms2version.py --onlycheck $PROJECT $MILESTONE
+  fi
+fi
+echo "$SERIES $RC (milestone $MILESTONE) is version $VERSION"
+
+title "Cloning repository for $PROJECT"
+MYTMPDIR=`mktemp -d`
+cd $MYTMPDIR
+git clone git://git.openstack.org/openstack/$PROJECT -b proposed/$SERIES
+cd $PROJECT
+git review -s
+
+if [ "$RC" == "final" ]; then
+  TAGMSG="${PROJECT^} $VERSION"
+else
+  TAGMSG="${PROJECT^} $MILESTONE milestone ($VERSION)"
+fi
+title "Tagging $VERSION ($TAGMSG)"
+git tag -m "$TAGMSG" -s "$VERSION"
+SHA=`git show-ref -s "$VERSION"`
+git push gerrit $VERSION
+
+title "Waiting for tarball from $SHA"
+$TOOLSDIR/wait_for_tarball.py $SHA
+
+title "Checking tarball is similar to last proposed-$SERIES.tar.gz"
+$TOOLSDIR/similar_tarballs.sh $PROJECT proposed-$SERIES $VERSION
+read -sn 1 -p "Press any key to continue..."
+
+title "Uploading tarball to Launchpad"
+if [ "$RC" == "final" ]; then
+  $TOOLSDIR/upload_release.py $PROJECT $VERSION
+else
+  $TOOLSDIR/upload_release.py $PROJECT $VERSION --milestone=$MILESTONE
+fi
+
+title "Cleaning up"
+cd ../..
+rm -rf $MYTMPDIR
