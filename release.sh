0355df1a7d2758fd8ab1720d9ba2331f9ae1211a
Doug Hellmann <doug@doughellmann.com>
2015-11-30 22:16:23 +0000

    add the series to the release tag
    Change-Id: I7453b632ebd384dc2105e4e988af9da90019f1f8

--- release.sh
+++ release.sh
@@ -63,7 +63,10 @@ title "Tagging $TARGETSHA as $VERSION"
 if git show-ref "$VERSION"; then
     echo "$REPO already has a version $VERSION tag"
 else
-    TAGMSG="$SHORTNAME $VERSION $RELEASETYPE"
+    # WARNING(dhellmann): announce.sh expects to be able to parse this
+    # commit message, so if you change the format you may have to
+    # update announce.sh as well.
+    TAGMSG="$SHORTNAME $SERIES $VERSION $RELEASETYPE"
     echo "Tag message is '$TAGMSG'"
     git tag -m "$TAGMSG" -s "$VERSION" $TARGETSHA
     git push gerrit $VERSION
