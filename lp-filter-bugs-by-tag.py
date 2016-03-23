a37dc9c627e47944f0d2cbdf0ad40b8f45ca0ab3
Ihar Hrachyshka <ihrachys@redhat.com>
2016-03-23 14:53:09 +0100

    Added tool to filter out bugs with specific tag
    Sometimes you have a list of bugs, and you need to filter out bugs that
    have some specific tag. For example, if you consider bugs for backports
    to a branch, you probably want to skip those bugs that already have
    in-stable-<release> tag set.
    
    And that's the use case for the filtering tool.
    
    The tool can be combined with other tools that output the list of bug
    numbers, like bugs-fixed-since.py or lp-filter-bugs-by-importance.py
    using standard Unix pipelines.
    
    Change-Id: I66e84b175c2a4efc87d897ac6d861c38b2ddfb3d

---
+++ lp-filter-bugs-by-tag.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""
+This Launchpad tool is used to filter out bugs by tag.
+"""
+
+import argparse
+import sys
+
+from launchpadlib.launchpad import Launchpad
+
+
+def _parse_args():
+    parser = argparse.ArgumentParser(
+        description='Filter out bugs with specified tag.')
+    parser.add_argument(
+        'project', help='Launchpad project name')
+    parser.add_argument(
+        'tag', nargs='?',
+        help='Tag to filter')
+    return parser.parse_args()
+
+
+def _filter_bugs(lp, project, tag, bugs):
+    return [
+        bug
+        for bug in bugs
+        for task in lp.bugs[bug].bug_tasks
+        if task.bug_target_name == project and tag not in lp.bugs[bug].tags
+    ]
+
+
+def main():
+    args = _parse_args()
+
+    lp = Launchpad.login_with('filter-bugs-by-tag', 'production')
+
+    bugs = [line.strip() for line in sys.stdin.readlines()]
+    bugs = _filter_bugs(lp, args.project, args.tag, bugs)
+    print(' '.join(bugs))
+
+
+if __name__ == '__main__':
+    main()
