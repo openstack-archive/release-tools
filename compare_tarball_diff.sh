d5f4ecd9dae808c66450867f60147c4a7f238839
Davanum Srinivas <davanum@gmail.com>
2016-04-18 09:31:04 -0400

    [WIP] Compare local and remote tarballs
    Change-Id: I6c06e76d059454bc6c8e424d07ca1bd7943a0e89

---
+++ compare_tarball_diff.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# Script to check the difference between produced and uploaded tarballs
+#
+# Copyright 2011-2013 Thierry Carrez <thierry@openstack.org>
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+function error {
+    echo ERROR: $1
+    rm -rf $TMPDIR
+    exit 1
+}
+
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 projectname branchname"
+    exit 2
+fi
+
+TMPDIR=`mktemp -d`
+git clone -b $2 https://git.openstack.org/openstack/$1 $TMPDIR/repo || error "clone failed"
+cd $TMPDIR/repo
+python setup.py sdist > /dev/null 2>&1 || error "sdist failed"
+cd dist/
+DIST_FILE=$(ls -1 *.tar.gz | head -1)
+wget -O remote.tar.gz http://tarballs.openstack.org/$1/$DIST_FILE
+mkdir local/
+tar -C local/ -xzf $DIST_FILE
+mkdir remote/
+tar -C remote/ -xzf remote.tar.gz
+diff -Bbwurd local/ remote/
