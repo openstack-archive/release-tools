8650b98fad7e0dfa08c398bca9e9bf763bc6fd58
Doug Hellmann <doug@doughellmann.com>
2016-06-22 10:53:07 -0400

    do not show empty change lists
    If there are no changes, do not show the header and an empty list.
    
    If there are no diff stats, do not show that section.
    
    Change-Id: Icfa1c8bcb954a4dd870a45bec7b4160df6ccf842
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

--- releasetools/release_notes.py
+++ releasetools/release_notes.py
@@ -104,7 +104,7 @@ For more details, please see below.
 
 # This will just be replaced with template values (no wrapping applied).
 CHANGE_RELEASE_TPL = """{% if reno_notes %}{{ reno_notes }}{% endif %}
-{{ change_header }}{% if skip_requirement_merges %}
+{% if changes %}{{ change_header }}{% if skip_requirement_merges %}
 
 NOTE: Skipping requirement commits...
 {%- endif %}
@@ -112,6 +112,8 @@ NOTE: Skipping requirement commits...
 {% for change in changes -%}
 {{ change }}
 {% endfor %}
+{%- endif %}
+{% if diff_stats %}
 {% if not first_release -%}
 Diffstat (except docs and test files)
 -------------------------------------
@@ -119,6 +121,7 @@ Diffstat (except docs and test files)
 {% for change in diff_stats -%}
 {{ change }}
 {% endfor %}
+{%- endif %}
 {% if requirement_changes %}
 Requirements updates
 --------------------
