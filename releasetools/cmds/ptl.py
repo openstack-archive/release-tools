3103c33fa7907bbf8a346e4978217c95f0cdcf49
Doug Hellmann <doug@doughellmann.com>
2016-06-24 15:35:41 -0400

    have the ptl command parse the liaison wiki page
    Add information about the release liaison to the output of the ptl
    command.
    
    Change-Id: Ie280420d89d4cbef08afc4b32d3551c4d9f46d57
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

--- releasetools/cmds/ptl.py
+++ releasetools/cmds/ptl.py
@@ -14,15 +14,66 @@
 
 from __future__ import print_function
 import argparse
+import itertools
 
 from releasetools import governance
+
+import mwclient
+
 
 _TEMPLATE = '''
 Name        : {name}
 IRC Nick    : {irc}
 IRC Channel : {irc_channel}
 Email       : {email}
+Liaison     : {liaison_name}
+              {liaison_irc}
 '''
+
+
+def get_page_section(page_content, section):
+    "Return iterable of lines making up a section of a wiki page."
+    section_start = u'== {} =='.format(section).lower()
+    lines = page_content.splitlines()
+    lines = itertools.dropwhile(
+        lambda x: x.lower() != section_start,
+        lines,
+    )
+    lines.next()  # skip the section heading
+    lines = itertools.takewhile(
+        lambda x: not x.startswith('== '),
+        lines,
+    )
+    return lines
+
+
+def get_wiki_table(page_content, section):
+    """Return iterable of dicts making up rows of a wiki table.
+
+    Assumes there is only one table per section.
+
+    """
+    lines = get_page_section(page_content, section)
+    lines = itertools.dropwhile(
+        lambda x: x != '{| class="wikitable"',
+        lines,
+    )
+    headings = []
+    for line in lines:
+        if line == '|-':
+            continue
+        elif line.startswith('!'):
+            headings = [h.strip() for h in line.lstrip('!').split('!!')]
+        elif line.startswith('|'):
+            items = [i.strip() for i in line.lstrip('|').split('||')]
+            row = {
+                h: i
+                for (h, i) in itertools.izip(headings, items)
+            }
+            yield row
+        elif line == '}':
+            # end of table
+            break
 
 
 def main():
@@ -40,15 +91,28 @@ def main():
         for n, i in team_data.items()
     }
 
+    # Check for liaison information.
+    site = mwclient.Site('wiki.openstack.org')
+    page = site.Pages['CrossProjectLiaisons']
+    table = get_wiki_table(page.text(), 'Release management')
+    liaisons = {
+        row['Project'].lower(): row
+        for row in table
+    }
+    team_liaison = liaisons.get(args.team.lower(), {})
+
     try:
         team = teams[args.team.lower()]
     except KeyError:
-        print('ERROR: No team {!r}'.format(args.team))
-    else:
-        ptl = team['ptl']
-        print(_TEMPLATE.format(
-            name=ptl.get('name'),
-            irc=ptl.get('irc'),
-            email=ptl.get('email'),
-            irc_channel=team.get('irc-channel'),
-        ))
+        print('No official team {!r}'.format(args.team))
+        team = {}
+
+    ptl = team['ptl']
+    print(_TEMPLATE.format(
+        name=ptl.get('name'),
+        irc=ptl.get('irc'),
+        email=ptl.get('email'),
+        irc_channel=team.get('irc-channel'),
+        liaison_name=team_liaison.get('Liaison'),
+        liaison_irc=team_liaison.get('IRC Handle'),
+    ))
